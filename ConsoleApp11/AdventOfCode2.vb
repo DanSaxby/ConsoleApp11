8ec929767891d8b5097958bf1923084d4677386a
Dan Saxby <Dan@dansaxby.me.uk>
2020-12-09 11:31:15 +0000

    advent od code3

--- ConsoleApp11/AdventOfCode2.vb
+++
@@ -1,3 +0,0 @@
-﻿Module AdventOfCode2
-
-End Module
--- ConsoleApp11/MainAOC.vb
+++ ConsoleApp11/MainAOC.vb
@@ -1,35 +1,50 @@
 ﻿Imports System.IO
 Module MainAOC
 
-    Sub main()
-        Console.WriteLine("What day of advent of code do you want to run?")
-        Dim choice As Integer = Console.ReadLine()
-        Select Case choice
-            Case 1
-            Case 2
-            Case 3
-            Case 4
-            Case 5
-            Case 6
-            Case 7
-            Case 8
-            Case 9
-            Case 10
-            Case 11
-            Case 12
-            Case 13
-            Case 14
-            Case 15
-            Case 16
-            Case 17
-            Case 18
-            Case 19
-            Case 20
-            Case 21
-            Case 22
-            Case 23
-            Case 24
-        End Select
+    Sub Main()
+        Dim linecount As Integer = File.ReadAllLines("AOC3.txt").Length
+        Dim TreeMap(30, (linecount - 1)) As Char
+        Dim line As String
+        Dim countx, county, i, treecount As Integer
+
+        Using sr As New StreamReader("AOC3.txt")
+            county = 0
+            For i = 0 To linecount - 1
+                line = sr.ReadLine()
+                countx = 0
+                For Each t In line
+                    TreeMap(countx, county) = t
+                    countx += 1
+                Next
+                county += 1
+            Next
+        End Using
+        countx = 0
+        county = 0
+        counttrees(1, 1, linecount, TreeMap)
+        counttrees(3, 1, linecount, TreeMap)
+        counttrees(5, 1, linecount, TreeMap)
+        counttrees(7, 1, linecount, TreeMap)
+        counttrees(1, 2, linecount, TreeMap)
+        Console.ReadLine()
+    End Sub
+    Sub counttrees(X As Integer, Y As Integer, linecount As Integer, treemap(,) As Char)
+        Dim countx, county, treecount As Integer
+        While county <> linecount
+            If county <= 323 Then
+
+                If countx > 30 Then
+                countx = countx - 31
+            End If
+                If treemap(countx, county) = "#" Then
+                    treecount += 1
+                End If
+
+            End If
+            countx += X
+            county += Y
+        End While
+        Console.WriteLine(treecount)
     End Sub
 
 
